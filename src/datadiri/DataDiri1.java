d88661fc9e9dab0135173094f97aeec954497dd8
damarwdsyh003 <53498693+damarwdsyh003@users.noreply.github.com>
2019-08-21 06:31:12 +0700

    first commit

---
+++ src/datadiri/DataDiri1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datadiri;
+
+/**
+ *
+ * @author Damar
+ */
+public class DataDiri1 {
+    public static void main(String[] args){
+        // membuat variabel
+        String nama, alamat, hobi, golongandarah;
+        int usia;
+        double tinggi;
+        
+        // mengisi variabel
+        nama = "muhammad_damar_widyansyah";
+        alamat = "perum bumimas blok s no 14 desa tunggulsari kedungwaru tulungagung";
+        golongandarah = "ab";
+        hobi = "gaming";
+        usia = 15;
+        tinggi = 165;
+        
+        // mencetak ke layar isi variabel
+        System.out.println("Nama: " + nama);
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Golongandarah: " + golongandarah);
+        System.out.println("Hobi: " + hobi);
+        System.out.println("Usia: " + usia + " tahun");
+        System.out.println("Tinggi: " + tinggi + " cm");
+    }
+
+}        
+        
+      
+        
+    
